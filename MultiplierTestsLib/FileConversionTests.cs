783627bc48d74f5f6fb8f886c16994d055129fa0
MarcoErice <marco.erice@gmail.com>
2017-09-11 15:13:35 +0200

    Added name convertion and Purchase Calculation

---
+++ MultiplierTestsLib/FileConversionTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiplierLib;
+
+namespace MultiplierTestsLib
+{
+    [TestClass]
+    public class FileConversionTests
+    {
+        [TestMethod]
+        public void marco_erice_1978()
+        {
+            var sut = new FileConverter();
+            string actual = sut.Transform("marco;erice;1978");
+            Assert.AreEqual("MARCO ERICE ÄR 39 ÅR GAMMAL.", actual);
+        }
+        [TestMethod]
+        public void annai_erice_2016()
+        {
+            var sut = new FileConverter();
+            string actual = sut.Transform("annai;erice;2016");
+            Assert.AreEqual("ANNAI ERICE ÄR 1 ÅR GAMMAL.", actual);
+        }
+       
+    }
+}
