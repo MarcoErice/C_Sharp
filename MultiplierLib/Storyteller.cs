708f188effde747e218ef8e7671c788fc6549db2
MarcoErice <marco.erice@gmail.com>
2017-09-13 14:09:19 +0200

    Started with Storyteller.

---
+++ MultiplierLib/Storyteller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplierLib
+{
+    public class Storyteller
+    {
+        public string TellStory(string nameOfChild, int ageOfChild, bool willBeScary)
+        {
+            
+            string story = "";
+            if (ageOfChild < 7)
+            {
+                story = $"Lyssna nu {nameOfChild}. Det var en gång en prinsessa ";
+            }
+            else
+                story = $"Hallå {nameOfChild}! En gång fanns en kille ";
+            
+            
+
+            string scary = "";
+            if (willBeScary == true)
+            {
+                scary = $"som egentligen var en varulv";
+            }
+            else
+                scary = $"som tyckte om att dansa";
+            
+            return story + scary;
+
+        }
+    }
+}
---
+++ MultiplierTestsLib/StorytellerTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MultiplierLib;
+
+namespace MultiplierTestsLib
+{
+    [TestClass]
+    public class StorytellerTests
+    {
+        [TestMethod]
+        public void TellStoryToYoungAnnaiNotScary()
+        {
+            // Arrange
+            var sut = new Storyteller();
+            // Act
+            var actual = sut.TellStory("Annai", 5, false);
+            // Assert
+            Assert.AreEqual("Lyssna nu Annai. Det var en gång en prinsessa " + "som tyckte om att dansa", actual);
+        }
+        [TestMethod]
+        public void TellStoryToYoungAnnaiScary()
+        {
+            // Arrange
+            var sut = new Storyteller();
+            // Act
+            var actual = sut.TellStory("Annai", 5, true);
+            // Assert
+            Assert.AreEqual("Lyssna nu Annai. Det var en gång en prinsessa " + "som egentligen var en varulv", actual);
+        }
+        [TestMethod]
+        public void TellStoryToOldMarcoNotScary()
+        {
+            // Arrange
+            var sut = new Storyteller();
+            // Act
+            var actual = sut.TellStory("Marco", 39, false);
+            // Assert
+            Assert.AreEqual("Hallå Marco! En gång fanns en kille " + "som tyckte om att dansa", actual);
+        }
+        [TestMethod]
+        public void TellStoryToOldMarcoScary()
+        {
+            // Arrange
+            var sut = new Storyteller();
+            // Act
+            var actual = sut.TellStory("Marco", 39, true);
+            // Assert
+            Assert.AreEqual("Hallå Marco! En gång fanns en kille " + "som egentligen var en varulv", actual);
+        }
+    }
+}
