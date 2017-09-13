5b15e040070ee1f03678e4e73f862d92d1cd1301
MarcoErice <marco.erice@gmail.com>
2017-09-13 10:32:39 +0200

    Updated ListHandler and ListhandlerTests.

--- MultiplierLib/ListHandler.cs
+++ MultiplierLib/ListHandler.cs
@@ -12,17 +12,21 @@ namespace MultiplierLib
 
         public void AddFiveStrings()
         {
-
+            List.Add("Idag är det Onsdag");
+            List.Add("Det är roligt med C#");
+            List.Add("Andra året på YH");
+            List.Add("Gäller och plugga hårt");
+            List.Add("Trevliga klasskompisar");
         }
 
         public void RemoveThirdString()
         {
-
+            List.RemoveAt(2);
         }
 
         public void Sort()
         {
-
+            List.Sort();
         }
     }
 }
