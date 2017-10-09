161307a16a2cfe262fc280b60c47c1b922516889
MarcoErice <marco.erice@gmail.com>
2017-10-09 13:59:54 +0200

    Updated EmployeeConverter

--- EmployeeLib/EmployeeConverter.cs
+++ EmployeeLib/EmployeeConverter.cs
@@ -9,22 +9,32 @@ namespace EmployeeLib
 {
     public class EmployeeConverter
     {
-        private string name;
-        public string Name {
+        private string myString;
+        public string MyString {
             get
             {
-                return name;
+                return myString;
             }
                 }
 
         public void Convert(string name)
         {
-            string pattern = @"\""?([^\""]*)\""? (\d{2}) (\d{5}.\d{2}) (\d{3}-\d{7})";
+            string pattern = @"\""?([^\""""]*)\""? \""?([^\""""]*)\""? (\d{2}) (\d{5}).(\d{2}) (\d{3}-\d{7})";
             string input = @"""Marco Erice"" 39 20200.00 076-2135921";
 
             Match m = Regex.Match(input, pattern);
 
-            var fullName = m.Groups[1].Value;
+            var firstName = m.Groups[1].Value;
+            var lastName = m.Groups[2].Value;
+            var bornYear = DateTime.Today.Year - int.Parse(m.Groups[3].Value);
+            var salary1 = m.Groups[4].Value;
+            var salary2 = m.Groups[5].Value;
+            var phone = m.Groups[6].Value;
+
+
+            this.myString = $"{lastName}, {firstName} " +
+                $"(Lön: {salary1},{salary2} SEK) Telefon:{phone} " +
+                $"Födelseår:{bornYear}";
         }
     }
 }
--- RegExTests/UnitTest1.cs
+++ RegExTests/UnitTest1.cs
@@ -25,12 +25,12 @@ namespace RegExTests
         public void converter()
         {
             var sut = new EmployeeConverter();
-            sut.Convert("Marco Erice");
+            sut.Convert("Marco Erice 39 20200.00 076-2135921");
 
-            Assert.AreEqual("Erice, Marco", sut.Name);
-            //Assert.AreEqual("39", m.Groups[2].Value);
-            //Assert.AreEqual("20200.00", m.Groups[3].Value);
-            //Assert.AreEqual("076-2135921", m.Groups[4].Value);
+            Assert.AreEqual("Erice, Marco (Lön: 20200,00 SEK) " +
+                "Telefon:076-2135921 Födelseår:1978", 
+                sut.MyString);
+            
         }
     }
 }
