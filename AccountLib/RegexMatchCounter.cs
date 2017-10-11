8df0ef040169fb016dc74a1e47f9cd4f65a98c4e
MarcoErice <marco.erice@gmail.com>
2017-10-11 13:14:57 +0200

    Added RegexMatchCounter on SIEImport.

---
+++ AccountLib/RegexMatchCounter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AccountLib
+{
+    public class RegexMatchCounter
+    {
+        
+        private int counter;
+        
+        public object CountMatchingLines(string content, string regexPattern)
+        {
+            using(var reader = new StringReader(content))
+            while (true)
+            {
+                var line = reader.ReadLine();
+                    if (line == null)
+                        break;
+                    if (Regex.Match(line, regexPattern).Success)
+                    {
+                        counter++;
+                    }
+            }
+            return counter;
+        }
+    }
+}
--- SIEImportTests/SIEImportTester.cs
+++ SIEImportTests/SIEImportTester.cs
@@ -6,6 +6,7 @@ using System.Text.RegularExpressions;
 using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
+using AccountLib;
 
 namespace SIEImportTests
 {
@@ -28,7 +29,7 @@ namespace SIEImportTests
                 {
                     var accountId = match.Groups[1].Value;
                     var amount = decimal.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
-                    
+
                     if (accounts.ContainsKey(accountId))
                     {
                         accounts[accountId] += amount;
@@ -43,6 +44,48 @@ namespace SIEImportTests
                 Debug.WriteLine($"{entry.Key} {entry.Value.ToString("F2")}");
             }
             Debug.WriteLine(accounts.Sum(entry => entry.Value));
+        }
+
+        [TestMethod]
+        public void TwoPhases()
+        {
+            var accounts = new Dictionary<string, decimal>();
+            var streamReader = File.OpenText(@"bokforing.txt");
+            while (true)
+            {
+                var line = streamReader.ReadLine();
+                if (line == null)
+                    break;
+                string pattern = @"#TRANS (\d{4}) {} (-?\d*.\d*)";
+                var match = Regex.Match(line, pattern);
+                if (match.Success)
+                {
+                    var accountId = match.Groups[1].Value;
+                    var amount = decimal.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
+
+                    if (accounts.ContainsKey(accountId))
+                    {
+                        accounts[accountId] += amount;
+                    }
+                    else
+                        accounts[accountId] = amount;
+                }
+
+            }
+            foreach (var entry in accounts.OrderBy(e => e.Key))
+            {
+                Debug.WriteLine($"{entry.Key} {entry.Value.ToString("F2")}");
+            }
+            Debug.WriteLine(accounts.Sum(entry => entry.Value));
+        }
+        [TestMethod]
+        public void RegexMatchCounter()
+        {
+            var content = File.ReadAllText(@"bokforing.txt");
+            string pattern = @"#VER";
+            var sut = new RegexMatchCounter();
+            var lineCount = sut.CountMatchingLines(content, pattern);
+            Assert.AreEqual(66, lineCount);
         }
     }
 }
