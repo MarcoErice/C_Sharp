1d3504a8b05d41f3ad06a5b70fc85131f0206ab4
MarcoErice <marco.erice@gmail.com>
2017-10-11 10:50:22 +0200

    Updated SIEImportTests --> CanRead TestMethod.

--- SIEImportTests/SIEImportTester.cs
+++ SIEImportTests/SIEImportTester.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SIEImportTests
 {
@@ -9,6 +15,34 @@ namespace SIEImportTests
         [TestMethod]
         public void CanRead()
         {
+            var accounts = new Dictionary<string, decimal>();
+            var streamReader = File.OpenText(@"bokforing.txt");
+            while (true)
+            {
+                var line = streamReader.ReadLine();
+                if (line == null)
+                    break;
+                string pattern = @"#TRANS (\d{4}) {} (-?\d*.\d*)";
+                var match = Regex.Match(line, pattern);
+                if (match.Success)
+                {
+                    var accountId = match.Groups[1].Value;
+                    var amount = decimal.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
+                    
+                    if (accounts.ContainsKey(accountId))
+                    {
+                        accounts[accountId] += amount;
+                    }
+                    else
+                        accounts[accountId] = amount;
+                }
+
+            }
+            foreach (var entry in accounts.OrderBy(e => e.Key))
+            {
+                Debug.WriteLine($"{entry.Key} {entry.Value.ToString("F2")}");
+            }
+            Debug.WriteLine(accounts.Sum(entry => entry.Value));
         }
     }
 }
