d038cef0691c6c3de35ba530c268b2e8955fec70
MarcoErice <marco.erice@gmail.com>
2017-10-18 10:43:23 +0200

    Updated Practice1

--- PracticeApp/Form1.Designer.cs
+++ PracticeApp/Form1.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.personNumber = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.phone = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // personNumber
+            // 
+            this.personNumber.Location = new System.Drawing.Point(226, 54);
+            this.personNumber.Name = "personNumber";
+            this.personNumber.Size = new System.Drawing.Size(252, 31);
+            this.personNumber.TabIndex = 0;
+            this.personNumber.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(226, 138);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(252, 31);
+            this.name.TabIndex = 1;
+            this.name.TextChanged += new System.EventHandler(this.name_TextChanged);
+            // 
+            // phone
+            // 
+            this.phone.Location = new System.Drawing.Point(226, 225);
+            this.phone.Name = "phone";
+            this.phone.Size = new System.Drawing.Size(252, 31);
+            this.phone.TabIndex = 2;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 77);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Spara till fil";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Personnummer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "För o efternamn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mobilnummer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1439, 680);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.phone);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.personNumber);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox personNumber;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox phone;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- PracticeApp/Form1.cs
+++ PracticeApp/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,33 @@ namespace PracticeApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var birthDate = personNumber.Text;
+            var personName = name.Text;
+            var thePhone = phone.Text;
+
+            saveFileDialog1.ShowDialog();
+            File.AppendAllText(saveFileDialog1.FileName, $"{birthDate};");
+            File.AppendAllText(saveFileDialog1.FileName, $"{personName};");
+            File.AppendAllText(saveFileDialog1.FileName, $"{thePhone}.");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void name_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
