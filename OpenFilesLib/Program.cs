fd8be4aaf3618c960d0aeca4b1440cc382b85573
MarcoErice <marco.erice@gmail.com>
2017-10-30 14:26:23 +0100

    Updated Open2FilesAndConvert

---
+++ OpenFilesLib/Program.cs
@@ -0,0 +1,30 @@
+﻿using Opens2FilesLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenFilesLib
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej, var god ange sökvägen till första filen");
+            var firstFile = Console.ReadLine(); // C:\Users\marco\Desktop\FirstFile.txt
+            var amountContent = File.ReadAllText(firstFile);
+            Console.WriteLine("Hej, var god ange sökvägen till andra filen");
+            var secondFile = Console.ReadLine(); // C:\Users\marco\Desktop\SecondFile.txt
+            var qtyContent = File.ReadAllText(secondFile);
+            
+            var twoFilesProduct = new TwoFilesProduct();
+           
+            Console.WriteLine($"{twoFilesProduct.Product(amountContent,qtyContent)}\n");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Opens2FilesLib/TwoFilesProduct.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opens2FilesLib
+{
+    public class TwoFilesProduct
+    {
+       public string Product(string amountFile, string qtyFile)
+        {
+            StringReader firstStrReader = new StringReader(amountFile);
+            string amountLine, qtyLine, output = "";
+            decimal product = 0M;
+
+            while (true)
+            {
+                amountLine = firstStrReader.ReadLine();
+                if (amountLine == null)
+                    break;
+                StringReader secondStrReader = new StringReader(qtyFile);
+                while (true)
+                {
+
+                    qtyLine = secondStrReader.ReadLine();
+                    if (qtyLine == null)
+                        break;
+                    var amount = decimal.Parse(amountLine, CultureInfo.InvariantCulture);
+                    var amountString = amount.ToString(CultureInfo.InvariantCulture);
+
+                    product = amount * decimal.Parse(qtyLine);
+                    var productString = product.ToString(CultureInfo.InvariantCulture);
+                    output += $"{amountString}x{decimal.Parse(qtyLine)}={productString}\n";
+                }
+            }
+
+            return output;
+        }
+    }
+}
