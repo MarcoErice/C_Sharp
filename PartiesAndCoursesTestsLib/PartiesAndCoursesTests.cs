1efa229114e1be1f6b7dafa8134c32deef1f321b
MarcoErice <marco.erice@gmail.com>
2017-11-01 14:08:52 +0100

    Added PartiesAndCourses.

---
+++ PartiesAndCoursesTestsLib/PartiesAndCoursesTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Linq;
+
+namespace PartiesAndCoursesTestsLib
+{
+    [TestClass]
+    public class PartiesAndCoursesTests
+    {
+        [TestMethod]
+        public void OneCourseTest()
+        {
+            var cSharpCourse = "C#;2018-01-01;2018-02-05";
+            var tokens = cSharpCourse.Split(';');
+            var aCourse = tokens[0];
+            var endDate = tokens[2];
+            var startDate = tokens[1];
+            var dateDiffrens = DateTime.Parse(endDate) - DateTime.Parse(startDate);
+
+            var actual = $"Kursen {aCourse} pågår i {dateDiffrens.TotalDays} dagar med följande deltagare:";
+            var expected = "Kursen C# pågår i 35 dagar med följande deltagare;";
+
+            Assert.AreEqual(expected, actual);
+                        
+        }
+
+        [TestMethod]
+        public void OneCourseTestWithParties()
+        {
+            var parties = "Alice;C#;SQL\nBob;SQL\nCarol;MVC;C#";
+            var partiesLines = parties.Split('\n');
+            var alumnies = "";
+            foreach (var line in partiesLines)
+            {
+                var lineTokens = line.Split(';').ToList();
+                string cSharp = "C#";
+                if (lineTokens.Contains(cSharp))
+                {
+                    alumnies += lineTokens[0] +", ";
+                    Debug.WriteLine(lineTokens[0]);
+                }
+            }
+            alumnies = alumnies.Substring(0, alumnies.Length - 2);
+            var cSharpCourse = "C#;2018-01-01;2018-02-05";
+            var cSharpTokens = cSharpCourse.Split(';');
+            var aCourse = cSharpTokens[0];
+            var endDate = cSharpTokens[2];
+            var startDate = cSharpTokens[1];
+            var dateDiffrens = DateTime.Parse(endDate) - DateTime.Parse(startDate);
+
+            var actual = $"Kursen {aCourse} pågår i {dateDiffrens.TotalDays} dagar med följande deltagare:\n{alumnies}";
+            var expected = "Kursen C# pågår i 35 dagar med följande deltagare:\nAlice, Carol";
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
---
+++ PartiesAndCoursesTestsLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PartiesAndCoursesTestsLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PartiesAndCoursesTestsLib")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("99392a95-2dde-4492-973a-d277a62bde65")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
