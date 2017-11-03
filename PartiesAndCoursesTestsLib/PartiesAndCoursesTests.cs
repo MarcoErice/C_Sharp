c436039f4b8eff7c62bf070dabd1e8902c902be8
MarcoErice <marco.erice@gmail.com>
2017-11-03 19:37:33 +0100

    Updated Unit test on PartiesAndCourses.

--- PartiesAndCoursesTestsLib/PartiesAndCoursesTests.cs
+++ PartiesAndCoursesTestsLib/PartiesAndCoursesTests.cs
@@ -96,6 +96,7 @@ namespace PartiesAndCoursesTestsLib
             }
             cSharpAlumnies = cSharpAlumnies.Substring(0, cSharpAlumnies.Length - 2);
             sqlAlumnies = sqlAlumnies.Substring(0, sqlAlumnies.Length - 2);
+            mvcAlumnies = mvcAlumnies.Substring(0, mvcAlumnies.Length - 2);
             var cSharpCourseData = "C#;2018-01-01;2018-02-05";
             var sqlCourseData = "SQL;2018-02-10;2018-03-25";
             var mvcCourseData = "MVC;2018-04-01;2018-05-15";
@@ -112,12 +113,19 @@ namespace PartiesAndCoursesTestsLib
             var sqlStartDate = sqlTokens[1];
             var sqlDateDifferens = DateTime.Parse(sqlEndDate) - DateTime.Parse(sqlStartDate);
 
-            
+            var mvcTokens = mvcCourseData.Split(';');
+            var mvcCourse = mvcTokens[0];
+            var mvcEndDate = mvcTokens[2];
+            var mvcStartDate = mvcTokens[1];
+            var mvcDateDifferens = DateTime.Parse(mvcEndDate) - DateTime.Parse(mvcStartDate);
+
             var actual = $"Kursen {cSharpCourse} pågår i {cSharpDateDifferens.TotalDays} dagar med följande deltagare:\n{cSharpAlumnies}\n\n" +
-                $"Kursen {sqlCourse} pågår i {sqlDateDifferens.TotalDays} dagar med följande deltagare:\n{sqlAlumnies}";
+                $"Kursen {sqlCourse} pågår i {sqlDateDifferens.TotalDays} dagar med följande deltagare:\n{sqlAlumnies}\n\n" +
+                $"Kursen {mvcCourse} pågår i {mvcDateDifferens.TotalDays} dagar med följande deltagare:\n{mvcAlumnies}";
 
             var expected = "Kursen C# pågår i 35 dagar med följande deltagare:\nAlice, Carol\n\n" +
-                "Kursen SQL pågår i 43 dagar med följande deltagare:\nAlice, Bob";
+                "Kursen SQL pågår i 43 dagar med följande deltagare:\nAlice, Bob\n\n" +
+                "Kursen MVC pågår i 44 dagar med följande deltagare:\nCarol";
 
             Assert.AreEqual(expected, actual);
 
